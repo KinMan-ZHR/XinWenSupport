6de954fb7a0e62ec1a463343a59c6ef7c775d394
KinMan Zhang <2376494700@qq.com>
2023-05-17 10:20:23 +0800

    新闻后端起步工程

---
+++ src/main/java/com/kinman/XinwenSupportApplication.java
@@ -0,0 +1,13 @@
+package com.kinman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinwenSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinwenSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kinman/XinwenSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinwenSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
